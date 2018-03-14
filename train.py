0617b30fc20f46277a962fbbf0d8a227c2ccbcf3
Ubuntu <ubuntu@ip-172-31-21-174.ec2.internal>
2018-03-14 17:13:03 +0000

    updated conv code

--- train.py
+++ train.py
@@ -2,15 +2,18 @@ import numpy as np
 import tensorflow as tf
 from sklearn.metrics import confusion_matrix
 from time import time
-
+import fast_gradient
 from include.data import get_data_set
 from include.model import model
+import scipy.misc
 
+alpha = 0.5
+beta = 1
 
 train_x, train_y, train_l = get_data_set()
 test_x, test_y, test_l = get_data_set("test")
 
-x, y, output, global_step, y_pred_cls = model()
+reg_x, reg_y, reg_output, reg_y_pred_cls, adv_x, adv_y, adv_output, global_step, adv_y_pred_cls, discr_reg_final, discr_adv_final, discr_reg_y, discr_adv_y = model()
 
 _IMG_SIZE = 32
 _NUM_CHANNELS = 3
@@ -18,14 +21,26 @@ _BATCH_SIZE = 128
 _CLASS_SIZE = 10
 _ITERATION = 10000
 _SAVE_PATH = "./tensorboard/cifar-10/"
+dropout = 0.5
 
+cross_discr_norm = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = discr_reg_final, labels = discr_reg_y))
+cross_discr_adv = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = discr_adv_final, labels = discr_adv_y))
+discr_loss = cross_discr_norm + cross_discr_adv
+discr_optimizer = tf.train.RMSPropOptimizer(learning_rate=1e-3).minimize(discr_loss, var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'discr'))
+correct_norm_discr = tf.equal(tf.argmax(discr_reg_y, 1), tf.argmax(discr_reg_final, 1))
+correct_adv_discr = tf.equal(tf.argmax(discr_adv_y, 1), tf.argmax(discr_adv_final, 1))
+discr_accuracy = (tf.reduce_mean(tf.cast(correct_norm_discr, tf.float32)) + tf.reduce_mean(tf.cast(correct_adv_discr, tf.float32)))/2
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=y))
+loss = alpha * tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=reg_output, labels=reg_y)) + (1 - alpha) * tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=adv_output, labels=adv_y)) - beta * cross_discr_adv 
+sm_norm = tf.nn.softmax(reg_output)
 optimizer = tf.train.RMSPropOptimizer(learning_rate=1e-3).minimize(loss, global_step=global_step)
+fgm_eps = tf.placeholder(tf.float32, ())
+fgm_epochs = tf.placeholder(tf.float32, ())
+adv_examples = fast_gradient.fgm(reg_x, reg_output, sm_norm, eps = fgm_eps, epochs = fgm_epochs)
 
-
-correct_prediction = tf.equal(y_pred_cls, tf.argmax(y, axis=1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+reg_correct_prediction = tf.equal(reg_y_pred_cls, tf.argmax(reg_y, axis=1))
+adv_correct_prediction = tf.equal(adv_y_pred_cls, tf.argmax(adv_y, axis=1))
+accuracy = (tf.reduce_mean(tf.cast(reg_correct_prediction, tf.float32)) + tf.reduce_mean(tf.cast(adv_correct_prediction, tf.float32)))/2
 tf.summary.scalar("Accuracy/train", accuracy)
 
 
@@ -49,22 +64,35 @@ def train(num_iterations):
     '''
         Train CNN
     '''
+    counter = 0
+    y_norm_labels = np.squeeze(np.stack([[np.array([1,0])] for _ in range(_BATCH_SIZE)], axis = 0))
+    
+    y_adv_labels = np.squeeze(np.stack([[np.array([0,1])] for _ in range(_BATCH_SIZE)], axis = 0))
     for i in range(num_iterations):
         randidx = np.random.randint(len(train_x), size=_BATCH_SIZE)
         batch_xs = train_x[randidx]
         batch_ys = train_y[randidx]
-
+	
+        output = sess.run(reg_output, feed_dict={reg_x: batch_xs})
+        softmax_norm = sess.run(sm_norm, feed_dict={reg_x: batch_xs})
+        adv_images = sess.run(adv_examples, feed_dict={reg_x: batch_xs, reg_output: output, sm_norm: softmax_norm, fgm_eps: 0.25, fgm_epochs: 1})
+        if counter % 500 == 0:
+                scipy.misc.imsave("image_normal_" + str(counter) + ".jpeg", batch_xs[0, :].reshape((32, 32, 3)))
+                scipy.misc.imsave("image_adv_" + str(counter) + ".jpeg", adv_images[0, :].reshape((32, 32, 3)))
+        counter+=1
         start_time = time()
-        i_global, _ = sess.run([global_step, optimizer], feed_dict={x: batch_xs, y: batch_ys})
+        i_global, _ = sess.run([global_step, optimizer], feed_dict={reg_x: batch_xs, reg_y: batch_ys, adv_x: adv_images, adv_y: batch_ys, discr_reg_y:y_norm_labels, discr_adv_y: y_adv_labels})
         duration = time() - start_time
-
+        sess.run(discr_optimizer, feed_dict={reg_x: batch_xs, adv_x: adv_images, discr_reg_y: y_norm_labels, discr_adv_y: y_adv_labels}) 
         if (i_global % 10 == 0) or (i == num_iterations - 1):
-            _loss, batch_acc = sess.run([loss, accuracy], feed_dict={x: batch_xs, y: batch_ys})
+            _loss, batch_acc = sess.run([loss, accuracy], feed_dict={reg_x: batch_xs, reg_y: batch_ys, adv_x: adv_images, adv_y: batch_ys, discr_reg_y:y_norm_labels, discr_adv_y: y_adv_labels})
+            discr_acc, disc_loss, test= sess.run([discr_accuracy, discr_loss, discr_reg_final], feed_dict={reg_x: batch_xs, adv_x: adv_images, discr_reg_y: y_norm_labels, discr_adv_y: y_adv_labels})
             msg = "Global Step: {0:>6}, accuracy: {1:>6.1%}, loss = {2:.2f} ({3:.1f} examples/sec, {4:.2f} sec/batch)"
             print(msg.format(i_global, batch_acc, _loss, _BATCH_SIZE / duration, duration))
+            print("Discriminator Accuracy (Loss): " + str(discr_acc) + " (" + str(disc_loss) + ")")
 
         if (i_global % 100 == 0) or (i == num_iterations - 1):
-            data_merged, global_1 = sess.run([merged, global_step], feed_dict={x: batch_xs, y: batch_ys})
+            data_merged, global_1 = sess.run([merged, global_step], feed_dict={reg_x: batch_xs, reg_y: batch_ys, adv_x:adv_images, adv_y: batch_ys})
             acc = predict_test()
 
             summary = tf.Summary(value=[
@@ -85,9 +113,10 @@ def predict_test(show_confusion_matrix=False):
     predicted_class = np.zeros(shape=len(test_x), dtype=np.int)
     while i < len(test_x):
         j = min(i + _BATCH_SIZE, len(test_x))
+
         batch_xs = test_x[i:j, :]
         batch_ys = test_y[i:j, :]
-        predicted_class[i:j] = sess.run(y_pred_cls, feed_dict={x: batch_xs, y: batch_ys})
+        predicted_class[i:j] = sess.run(reg_y_pred_cls, feed_dict={reg_x: batch_xs, reg_y: batch_ys})
         i = j
 
     correct = (np.argmax(test_y, axis=1) == predicted_class)
